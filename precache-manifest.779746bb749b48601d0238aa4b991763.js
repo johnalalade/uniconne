1d62399c3c905aa06a46ca0b19afc3ab9b2c039f
John Alalade <johnalalade6@gmail.com>
2021-03-17 11:35:48 -0400

    Updates

---
+++ precache-manifest.779746bb749b48601d0238aa4b991763.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b39f511a843f278f0de781094ca20f",
+    "url": "/uniconne/index.html"
+  },
+  {
+    "revision": "9c25e786411cf013fa89",
+    "url": "/uniconne/static/css/2.41fc952c.chunk.css"
+  },
+  {
+    "revision": "40c72eb3bf8f137399f4",
+    "url": "/uniconne/static/css/main.778fd273.chunk.css"
+  },
+  {
+    "revision": "9c25e786411cf013fa89",
+    "url": "/uniconne/static/js/2.9c70d3bd.chunk.js"
+  },
+  {
+    "revision": "0f7319871f92769dde3ac6419e675f2a",
+    "url": "/uniconne/static/js/2.9c70d3bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40c72eb3bf8f137399f4",
+    "url": "/uniconne/static/js/main.bde1e0c1.chunk.js"
+  },
+  {
+    "revision": "37bc3108bb47499e93de",
+    "url": "/uniconne/static/js/runtime-main.8627589b.js"
+  },
+  {
+    "revision": "2c27ec2fdb0ec929fec1b64d9e34ff66",
+    "url": "/uniconne/static/media/conne.2c27ec2f.jpeg"
+  },
+  {
+    "revision": "a4c35a7b261ac8a4084204b8ae07f601",
+    "url": "/uniconne/static/media/oau-bg.a4c35a7b.jpg"
+  },
+  {
+    "revision": "003926ca70ceae969fdd176dba549cbe",
+    "url": "/uniconne/static/media/oau-logo.003926ca.jpg"
+  },
+  {
+    "revision": "f78f56c7c7f97c1591026a0d096255f9",
+    "url": "/uniconne/static/media/oau-uniconne.f78f56c7.jpg"
+  },
+  {
+    "revision": "268275ce496da8340802b0c0a8a7ab28",
+    "url": "/uniconne/static/media/uniconne-logo.268275ce.jpg"
+  }
+]);
